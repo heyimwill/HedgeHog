32be6cd4c4bbe79896c5bc776622230a73291e91
Will <36234414+heyimwill@users.noreply.github.com>
2021-01-17 17:47:26 -0700

    Update DateAndActivityStack.cs

--- SOVP_SourceFiles/AP_Recover/AP_Recover/DateAndActivityStack.cs
+++ SOVP_SourceFiles/AP_Recover/AP_Recover/DateAndActivityStack.cs
@@ -2,7 +2,7 @@
 // Type: AccelerometerProcessing_FiveSecond.DateAndActivityStack
 // Assembly: AccelerometerProcessing_FiveSecond, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 // MVID: D74A3EC3-23D5-487A-8102-93D5769E4944
-// Assembly location: C:\Users\lauren.vonklinggraef\AppData\Local\Apps\2.0\5KCOR2G4.TRZ\N5LHNGBE.QAQ\acce..tion_c04be0470dfe34fc_0001.0000_388283ee07e1429b\AccelerometerProcessing_FiveSecond.exe
+// Assembly location: C:\Users\l\AppData\Local\Apps\2.0\5KCOR2G4.TRZ\N5LHNGBE.QAQ\acce..tion_c04be0470dfe34fc_0001.0000_388283ee07e1429b\AccelerometerProcessing_FiveSecond.exe
 
 namespace AccelerometerProcessing_FiveSecond
 {
